2de7d38aaab484d0c6abcbcbd7132a6ad485c6a7
Velmurugan Kuberan <vkuberan@gmail.com>
2020-12-16 23:29:54 +0530

    Map Function

--- map-func.py
+++ map-func.py
@@ -2,13 +2,19 @@ import random
 # Map Functionality
 
 
-def addition(a, b):
-    print("{} + {} = {}".format(a, b, a+b))
-    return a + b
+def print_iterator(header, it):
+    print(header)
+    print('*' * len(header))
+    for x in it:
+        print(x, end=' ')
+    print("\n\n")  # New Line
 
 
-result = map(addition, [i for i in range(1, 6)], [
-             random.randint(1, 999999) for i in range(6, 11)])
+result = map(lambda x, y: (x+y), [i for i in range(1, 20)], [
+    random.randint(1, 999999) for i in range(1, 20)])
 
-for i in result:
-    print(i)
+print_iterator("Addition: ", result)
+
+result = map(lambda x: x.upper(), [
+    'welcome', 'hello', 'world!!!', 'bye bye', '2020???'])
+print_iterator('Strings To Upper Case', result)
