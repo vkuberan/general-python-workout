c27250c618fa47e6492694d9a71d8b813d110473
Velmurugan Kuberan <vkuberan@gmail.com>
2020-12-21 23:13:27 +0530

    High Order Functions

--- high-order-workout.py
+++ high-order-workout.py
@@ -12,6 +12,10 @@ def squared(numbers):
     return [(i * i) for i in numbers]
 
 
+def cubed(n, j):
+    return [(x * x * x) for x in n]
+
+
 def high_order_func_demo(transform, numbers):
     return transform(numbers)
 
@@ -21,3 +25,4 @@ numbers = [1, 2, 3, 4, 5]
 result = high_order_func_demo(squared, numbers)
 print(numbers)
 print(result)
+print(high_order_func_demo(cubed, numbers))
