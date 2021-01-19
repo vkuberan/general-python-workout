3c4a08db445c16ed2db3239c13f547e5c0cf430d
Velmurugan Kuberan <vkuberan@gmail.com>
2021-01-19 10:43:24 +0530

    Project Euler

--- project-euler/problem-1.py
+++ project-euler/problem-1.py
@@ -3,6 +3,7 @@
 
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
+# First method and very simple one
 total = 0
 
 for i in range(1, 1000):
@@ -11,6 +12,11 @@ for i in range(1, 1000):
 
 print(total)
 
+# This is second one and uses a iterable function sum
 total = sum(i for i in range(1, 1000) if ((i % 3 == 0) or (i % 5 == 0)))
 
+print(total)
+
+# This one is more interesting
+total = sum({*range(3, 1000, 3)} | {*range(5, 1000, 5)})
 print(total)
