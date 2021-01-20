380dcd994c0646059e6aa81355682b4048b82018
Velmurugan Kuberan <vkuberan@gmail.com>
2021-01-21 04:57:46 +0530

    Leanrning Scrapy

--- learning-scraping/cities.py
+++ learning-scraping/cities.py
@@ -1,10 +1,38 @@
+import time
 from bs4 import BeautifulSoup
 import requests
 
-with open('cities.html') as html_file:
-    soup = BeautifulSoup(html_file, "lxml")
+time_taken = {}
 
-# print(soup.prettify())
+with open('cities.html') as html_file:
+
+    # html.parser
+    start_time = time.time()
+    soup = BeautifulSoup(html_file, "html.parser")
+    end_time = time.time()
+    time_diff = end_time - start_time
+    time_taken["html.parser"] = time_diff
+
+with open('cities.html') as html_file:
+    # lxml
+    start_time = time.time()
+    soup = BeautifulSoup(html_file, "lxml")
+    end_time = time.time()
+    time_diff = end_time - start_time
+    time_taken["lxml"] = time_diff
+
+with open('cities.html') as html_file:
+    # html5lib
+    start_time = time.time()
+    soup = BeautifulSoup(html_file, "html5lib")
+    end_time = time.time()
+    time_diff = end_time - start_time
+    time_taken["html5lib"] = time_diff
+
+
+for key, val in time_taken.items():
+    print("{} {}".format(key, val))
+
 
 title = soup.title
 title_text = soup.title.text
