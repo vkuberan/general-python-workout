a4ffda4c5b883c2bc85103b39d64e647a7fc3ec7
Velmurugan Kuberan <vkuberan@gmail.com>
2021-01-24 07:16:10 +0530

    Extract information from wiki using BeautifulSoup and requests

--- learning-scraping/scrape-wiki/wec-bu.py
+++ learning-scraping/scrape-wiki/wec-bu.py
@@ -1,4 +1,5 @@
 from common import *
+import time
 import json
 from bs4 import BeautifulSoup
 import requests
@@ -13,7 +14,7 @@ project_dirs = {
 create_related_dirs(project_dirs)
 
 data_dir = 'wiki/data/all-countries.json'
-base_wiki_url = 'https://en.wikipedia.org/'
+base_wiki_url = 'https://en.wikipedia.org'
 wiki_countries_entry = {}
 
 with open(data_dir, 'r') as fp:
@@ -30,13 +31,32 @@ for entry, country_info in wiki_countries_entry.items():
         entry)
     print_char_under_string(commonStr)
 
-    link_source = properties[0]
-    html_file = '/'.join([project_dirs['html_dir'], properties[1]])
-    data_file = '/'.join([project_dirs['data_dir'], properties[2]])
+    link_source = ''.join([base_wiki_url, country_info[1]])
+    html_file = '/'.join([project_dirs['html_dir'], country_info[3]])
+    data_file = '/'.join([project_dirs['data_dir'], country_info[4]])
 
     commonStr = "Wikipedia Link: {}, Files: [HTML: {}, Data: {}]".format(
         link_source, html_file, data_file)
     print_char_under_string(commonStr, '-')
 
-    input("\nPress any key to continue...")
+    try:
+        with open(html_file, 'rb') as hs:
+            html_source = hs.read().decode("UTF-8")
+            print_char_under_string(
+                "Fetching info from the crawled file.", '-', '\n')
+            soup = BeautifulSoup(html_source, 'lxml')
+            # print(soup.prettify())
+    except:
+        print_char_under_string(
+            "Fetching data from the server using request.", '-', '\n')
+        res = requests.get(link_source)
+        soup = BeautifulSoup(res.text, 'lxml')
+        f = open(html_file, mode='w', encoding='UTF-8')
+        f.write(res.text)
+        f.close()
+        time.sleep(3)
+
+    print(soup.title.text)
+
+    # input("\nPress any key to continue...")
     clear_screen()
